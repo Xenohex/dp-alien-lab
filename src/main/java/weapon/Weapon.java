1d5a6fcfd23eb66cccaee486192e096a563c8ada
Christian Spitler <cs5086@cs.ship.edu>
2021-10-11 22:15:09 -0400

    Added methods to Weapon interface

--- src/main/java/weapon/Weapon.java
+++ src/main/java/weapon/Weapon.java
@@ -1,5 +1,15 @@
 package weapon;
 
 public interface Weapon {
-
+  
+  public int fire(int distance);
+  public int getBaseDamage();
+  public int getMaxRange();
+  public int getRateOfFire();
+  public int getMaxAmmo();
+  public int getCurrentAmmo();
+  public int getShotsLeft();
+  public int getNumAttachments();
+  public void reload();
+  public String toString();
 }
