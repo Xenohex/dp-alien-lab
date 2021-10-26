3f0644795e602ea91e0747f382fd046e6799e6a2
AP <ap8164@engr.ship.edu>
2021-10-26 12:49:31 -0400

    fixed checkstyle and optimized getDistance

--- src/main/java/exceptions/EnvironmentException.java
+++ src/main/java/exceptions/EnvironmentException.java
@@ -1,6 +1,6 @@
 package exceptions;
 
-public class EnvironmentException extends Exception{
+public class EnvironmentException extends Exception {
   public EnvironmentException(String message) {
     super(message);
   }
