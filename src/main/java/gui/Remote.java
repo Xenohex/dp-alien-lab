e88500eb656d033e9796f519c0045208263e9634
AP <ap8164@engr.ship.edu>
2021-11-16 14:38:20 -0500

    fixed checkStyle for the remote class while maintaining the testing additions from a previous commit

--- src/main/java/gui/Remote.java
+++ src/main/java/gui/Remote.java
@@ -1,7 +1,18 @@
 package gui;
 
+/**
+ * 
+ * @author Austin Pliska
+ * Remote to set and execute commands in order to satisfy command pattern
+ *
+ */
 public class Remote {
   Command c;
-  public void setCommand(Command cc) { c = cc; }
-  public void buttonPressed() { c.execute(); }
+  public void setCommand(Command cc) {
+    c = cc;
+  }
+  
+  public void buttonPressed() {
+    c.execute();
+  }
 }
