30fbd177af0f11dd7a24c190f2af122a2fff6786
Badreddine <bb6848@ship.edu>
2021-11-30 17:23:56 -0500

    Added updateCell in Dead state

--- src/main/java/state/DeadState.java
+++ src/main/java/state/DeadState.java
@@ -1,6 +1,8 @@
 package state;
 
 import environment.Cell;
+import environment.Environment;
+import lifeform.LifeForm;
 import weapon.Weapon;
 
 public class DeadState extends ActionState {
@@ -11,17 +13,23 @@ public class DeadState extends ActionState {
 
   @Override
   void executeAction() {
+    LifeForm lifeForm = context.getLifeForm();
+    Environment e = context.getEnvironment();
     respawn();
     context.setCurrentState(context.getNoWeaponState());
+    e.updateCell(lifeForm.getRow(), lifeForm.getCol());
   }
 
   public void respawn() {
+    LifeForm lifeForm = context.getLifeForm();
+    Environment e = context.getEnvironment();
     Cell cell = context.e.getRandomCell();
     Weapon w;
     if(lifeForm.hasWeapon() && cell.getWeaponsCount() != 2 && cell.getLifeForm() == null) {
       cell.addLifeForm(lifeForm);
       w = lifeForm.dropWeapon();
       e.addWeapon(w, lifeForm.getRow(), lifeForm.getCol());
+      e.updateCell(lifeForm.getRow(), lifeForm.getCol());
     } else {
       System.out.println("No space available in this cell to drop the weapon!");
     }
